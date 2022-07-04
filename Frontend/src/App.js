a2cde1c49755180a6e0605a8b98916405cbb5f9b
Andreis Purim <andreispurim@gmail.com>
2022-07-04 16:19:41 +0300

    Changed app to react hooks

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -56,79 +56,42 @@ class ApiMDC{
   }
 }
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      server_url: "http://localhost:8888/",
-      server_key: "1033b11e-ea82-11ec-8fea-0242ac120002",
-      email: "mail@mail.com_",
-      name: "",
-      user_uid: "",
-
-      step: "server_info",// server_info, user_info
-      btn_label: "Próximo"
-    };
-    
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleInputChange(event) {
+export default function App(props){
+  const handleInputChange = event => {
     const target = event.target;
     const name = target.name;
-
-    this.setState({
-      [name]: target.value
-    });
+    props.setInfo({...props.info, [name]: target.value});
   }
-
-  render() {
-    if (this.state.step==="server_info"){
-      return this.render_server_info()
-    }else if (this.state.step==="user_info_criar"){
-      return this.render_user_info_new_user()
-    }else if (this.state.step==="user_info_confirmar_old"){
-      return this.render_user_info_old_user()
-    }
-  }
-
-  handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    
-
-    if (this.state.step==="server_info"){
-        const api = new ApiMDC(this.state.server_url, this.state.server_key)
-        const dis = this;
-        api.check_email(this.state.email).then(function(resp){
+    if (props.info.step==="server_info"){
+        const api = new ApiMDC(props.info.server_url, props.info.server_key)
+        api.check_email(props.info.email).then(function(resp){
           if(resp.status===200){
             if(resp.data.status){
-              dis.setState({'user_uid': resp.data.uid})
-              dis.setState({'name': resp.data.name})
-              dis.enable_confirm_account();
+              props.setInfo({...props.info, 'user_uid': resp.data.uid});
+              props.setInfo({...props.info, 'name': resp.data.name});
+              props.setInfo({...props.info, 'step': "user_info_confirmar_old"});
             }else{
-              dis.enable_create_account();
+              props.setInfo({...props.info, 'step': "user_info_criar"});
             }
           }else{
             alert("Not able to connect with credentials and endpoint.")
           }
         })
-    }else if(this.state.step==="user_info_confirmar_old"){
-      this.access_chat()
-    }else if(this.state.step==="user_info_criar"){
-      this.create_account()
+    }else if(props.info.step==="user_info_confirmar_old"){
+      props.setInfo({...props.info, 'view': 1});
+    }else if(props.info.step==="user_info_criar"){
+      create_account()
     }
   }
-
-  create_account(){
-    const api = new ApiMDC(this.state.server_url, this.state.server_key)
-    const dis = this;
-    api.create_account(this.state.email, this.state.name).then(function(resp){
+  const create_account = () => {
+    const api = new ApiMDC(props.info.server_url, props.info.server_key)
+    api.create_account(props.info.email, props.info.name).then(function(resp){
       if(resp.status===200){
         if(resp.data.status==="user_created"){
-          dis.setState({'user_uid': resp.data.uid})
-          dis.access_chat()
+          props.setInfo({...props.info, 'user_uid': resp.data.uid});
+          props.setInfo({...props.info, 'view': 1});
         }else{
           alert("Something went wrong. Check console for more informations")
           console.error(resp)
@@ -138,94 +101,49 @@ class App extends React.Component {
       }
     })
   }
-
-  access_chat(){
-
-  }
-
-  enable_confirm_account(){
-    this.setState({'name': 'nome nomenome'})
-    this.setState({'btn_label': 'Acessar'})
-    this.setState({'step': 'user_info_confirmar_old'})
-  }
-
-  enable_create_account(){
-    this.setState({'step': 'user_info_criar'})
-    this.setState({'btn_label': 'Criar e Acessar'})
-  }
-
-  render_server_info(){
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          E-mail
-          <input
-            name="email"
-            type="text"
-            value={this.state.email}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Server Endpoint
-          <input
-            name="server_url"
-            type="text"
-            value={this.state.server_url}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          Server Key
-          <input
-            name="server_key"
-            type="text"
-            value={this.state.server_key}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <input type="submit" value={this.state.btn_label} />
-      </form>
-    );
-  }
-
-
-  render_user_info_new_user(){
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <p>Usuário não encontrado! Cadastre para acessar</p>
-        <label>
-          Nome
-          <input
-            name="name"
-            type="text"
-            value={this.state.name}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <input type="submit" value={this.state.btn_label} />
-      </form>
-      );
-  }
-
-  render_user_info_old_user(){
-
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <p>Usuário {this.state.email} encontrado!</p>
-        <p>Confirme seu nome de usuário</p>
-        <label>
-          Nome
-          <input
-            name="name"
-            type="text"
-            value={this.state.name} disabled/>
-        </label>
-        <br />
-        <input type="submit" value={this.state.btn_label} />
-      </form>
-      );
-  }
+  return(
+    <div>
+      {props.info.step==="server_info"?
+        <form onSubmit={handleSubmit}>
+          <label>
+            E-mail
+            <input name="email" type="text" value={props.info.email} onChange={handleInputChange} />
+          </label>
+          <br/>
+          <label>
+            Server Endpoint
+            <input name="server_url" type="text" value={props.info.server_url} onChange={handleInputChange} />
+          </label>
+          <br/>
+          <label>
+            Server Key
+            <input name="server_key" type="text" value={props.info.server_key} onChange={handleInputChange} />
+          </label>
+          <br/>
+          <input type="submit" value={props.info.btn_label} />
+        </form>
+      :props.info.step==="user_info_criar"?
+        <form onSubmit={handleSubmit}>
+          <p>Usuário não encontrado! Cadastre para acessar</p>
+          <label>
+            Nome
+            <input name="name" type="text" value={props.info.name} onChange={handleInputChange} />
+          </label>
+          <br/>
+          <input type="submit" value={props.info.btn_label} />
+        </form>
+      :
+        <form onSubmit={handleSubmit}>
+          <p>Usuário {props.info.email} encontrado!</p>
+          <p>Confirme seu nome de usuário</p>
+          <label>
+            Nome
+            <input name="name" type="text" value={props.info.name} disabled/>
+          </label>
+          <br/>
+          <input type="submit" value={props.info.btn_label} />
+        </form>
+      }
+    </div>
+  )
 }
-
-export default App;
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,16 +4,31 @@ import './index.css';
 import App from './App';
 import Chat from './Chat';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export default function ChatApp(){
+    const [info, setInfo] = React.useState({
+        view: 0,
+        server_url: "http://localhost:8888/",
+        server_key: "1033b11e-ea82-11ec-8fea-0242ac120002",
+        email: "mail@mail.com_",
+        name: "nome",
+        user_uid: "33b46c3b-0299-4459-924s-ab5ba7f67f1a",
+        step: "server_info",// server_info, user_info
+        btn_label: "Próximo",
+
+        status: 'first',
+        max_time: null,
+        story: null,
+        story_id: null,
+  
+        message: '',
+    });
+    const sendInfo = {info, setInfo}
+    switch(info.view){
+        case 0: return <App {...sendInfo}/>
+        case 1: return <Chat {...sendInfo}/>
+    }
+    
+}
 
 
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-
-root.render(
-    <Chat />
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<ChatApp />)
